5f327c1186e55aa6c294afb2487a9c319e6110ef
Deepakpulluru <p.deepak5188@gmail.com>
2024-06-04 09:28:09 +0530

    Update wheather.py

--- Deepak Pulluru/wheather.py
+++ Deepak Pulluru/wheather.py
@@ -1,68 +1,51 @@
-# import required modules
-import requests, json
+import http.client
+import json
 
-# Enter your API key here
+def get_weather(api_key, city_name):
+    # Create a connection to the OpenWeatherMap API server
+    conn = http.client.HTTPConnection("api.openweathermap.org")
+    
+    # Build the complete URL with the API key and city name
+    url = f"/data/2.5/weather?appid={api_key}&q={city_name}"
+    
+    # Make the GET request
+    conn.request("GET", url)
+    
+    # Get the response
+    response = conn.getresponse()
+    
+    # Read the response data
+    data = response.read()
+    
+    # Decode the data from bytes to a string and then parse it as JSON
+    weather_data = json.loads(data.decode("utf-8"))
+    
+    # Check if the city is found (cod 200 means success)
+    if weather_data["cod"] == 200:
+        # Get the main data
+        main_data = weather_data["main"]
+        
+        # Extract temperature, pressure, and humidity
+        current_temperature = main_data["temp"]
+        current_pressure = main_data["pressure"]
+        current_humidity = main_data["humidity"]
+        
+        # Get the weather description
+        weather_description = weather_data["weather"][0]["description"]
+        
+        # Print the weather information
+        print(f"Temperature (in Kelvin): {current_temperature}")
+        print(f"Atmospheric pressure (in hPa): {current_pressure}")
+        print(f"Humidity (in percentage): {current_humidity}")
+        print(f"Weather description: {weather_description}")
+    else:
+        print("City Not Found")
+
+# Replace 'Your_API_Key' with your actual API key
 api_key = "Your_API_Key"
 
-# base_url variable to store url
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
+# Input city name from the user
+city_name = input("Enter city name: ")
 
-# Give city name
-city_name = input("Enter city name : ")
-
-# complete_url variable to store
-# complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-
-# get method of requests module
-# return response object
-response = requests.get(complete_url)
-
-# json method of response object 
-# convert json format data into
-# python format data
-x = response.json()
-
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to
-# "404", means city is found otherwise,
-# city is not found
-if x["cod"] != "404":
-
-	# store the value of "main"
-	# key in variable y
-	y = x["main"]
-
-	# store the value corresponding
-	# to the "temp" key of y
-	current_temperature = y["temp"]
-
-	# store the value corresponding
-	# to the "pressure" key of y
-	current_pressure = y["pressure"]
-
-	# store the value corresponding
-	# to the "humidity" key of y
-	current_humidity = y["humidity"]
-
-	# store the value of "weather"
-	# key in variable z
-	z = x["weather"]
-
-	# store the value corresponding 
-	# to the "description" key at 
-	# the 0th index of z
-	weather_description = z[0]["description"]
-
-	# print following values
-	print(" Temperature (in kelvin unit) = " +
-					str(current_temperature) +
-		"\n atmospheric pressure (in hPa unit) = " +
-					str(current_pressure) +
-		"\n humidity (in percentage) = " +
-					str(current_humidity) +
-		"\n description = " +
-					str(weather_description))
-
-else:
-	print(" City Not Found ")
+# Call the get_weather function
+get_weather(api_key, city_name)
